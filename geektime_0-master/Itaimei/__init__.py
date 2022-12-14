8cbd0248f5eb853fb250c9ed1d3888d6b3ff83fd
zhoufan55 <1107707279@qq.com>
2022-12-14 11:13:34 +0800

    first

---
+++ geektime_0-master/Itaimei/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/12/14 11:09
+# @Author : zhoufan
+# @Email : 13952047994@163.com
+# @File : __init__.py.py
+# @Project : geektime_zf_UI
---
+++ geektime_0-master/Itaimei/tests/test_login.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/12/14 11:11
+# @Author : zhoufan
+# @Email : 13952047994@163.com
+# @File : test_login.py
+# @Project : geektime_zf_UI
+
+
+class TestLogin(object):
+    def setup_class(self):
+        """
+        初始化浏览器
+        """
+        self.login = Login()
