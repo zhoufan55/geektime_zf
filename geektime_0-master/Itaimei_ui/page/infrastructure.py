5654a04ac24db90f52ccc8833bcb226584d44a86
zhoufan55 <1107707279@qq.com>
2022-12-21 18:05:09 +0800

    itamei1

--- geektime_0-master/Itaimei_ui/page/infrastructure.py
+++ geektime_0-master/Itaimei_ui/page/infrastructure.py
@@ -4,6 +4,9 @@
 # @Email : 13952047994@163.com
 # @File : report.py
 # @Project : geektime_0-master
+import logging
+import time
+
 from selenium.webdriver.common.by import By
 from Itaimei_ui.framework.basepage import BasePage
 
@@ -13,8 +16,8 @@ class Infrastructure(BasePage):
         super().__init__(driver)
 
     def draftManagement(self):
-        self.wait(By.CSS_SELECTOR, '.wrap___2wsUC > .edc-form-horizontal > div:nth-child(2)')
-        self.click(By.CSS_SELECTOR, '.wrap___2wsUC > .edc-form-horizontal > div:nth-child(2)')
+        self.wait(By.CSS_SELECTOR, '.edc-form-horizontal > div:nth-child(2)')
+        self.click(By.CSS_SELECTOR, '.edc-form-horizontal > div:nth-child(2)')
         self.wait(By.CSS_SELECTOR, '.menu___1g4__ > div:nth-child(1) > div:nth-child(1)')
         eles1 = len(self.findElements(By.CSS_SELECTOR, '.menu___1g4__ > div:nth-child(1) > div'))
         eles2 = len(self.findElements(By.CSS_SELECTOR, '.menu___1g4__ > div:nth-child(2) > div'))
@@ -54,22 +57,70 @@ class Infrastructure(BasePage):
         self.click(By.CSS_SELECTOR, '.menu___1g4__ > div:nth-child(1) > div:nth-child(6)')
 
     def toEcrfAccess(self):
-        ...
+        hover_ele = self.findElement(By.CSS_SELECTOR, '.menu___1g4__ > div:nth-child(2) > div:nth-child(1)')
+        self.action_move(hover_ele)
+        self.click(By.CSS_SELECTOR, '.menu___1g4__ > div:nth-child(2) > div:nth-child(1)')
 
     def toGuidelines(self):
-        ...
+        hover_ele = self.findElement(By.CSS_SELECTOR, '.menu___1g4__ > div:nth-child(2) > div:nth-child(2)')
+        self.action_move(hover_ele)
+        self.click(By.CSS_SELECTOR, '.menu___1g4__ > div:nth-child(2) > div:nth-child(2)')
 
     def toEditCheck(self):
-        ...
+        hover_ele = self.findElement(By.CSS_SELECTOR, '.menu___1g4__ > div:nth-child(2) > div:nth-child(3)')
+        self.action_move(hover_ele)
+        self.click(By.CSS_SELECTOR, '.menu___1g4__ > div:nth-child(2) > div:nth-child(3)')
 
     def toDerivations(self):
-        ...
+        hover_ele = self.findElement(By.CSS_SELECTOR, '.menu___1g4__ > div:nth-child(2) > div:nth-child(4)')
+        self.action_move(hover_ele)
+        self.click(By.CSS_SELECTOR, '.menu___1g4__ > div:nth-child(2) > div:nth-child(4)')
 
     def toAdvancedFunctions(self):
-        ...
+        hover_ele = self.findElement(By.CSS_SELECTOR, '.menu___1g4__ > div:nth-child(2) > div:nth-child(5)')
+        self.action_move(hover_ele)
+        self.click(By.CSS_SELECTOR, '.menu___1g4__ > div:nth-child(2) > div:nth-child(5)')
 
     def toMappingConfig(self):
-        ...
+        hover_ele = self.findElement(By.CSS_SELECTOR, '.menu___1g4__ > div:nth-child(2) > div:nth-child(6)')
+        self.action_move(hover_ele)
+        self.click(By.CSS_SELECTOR, '.menu___1g4__ > div:nth-child(2) > div:nth-child(6)')
+
+    def add_draft(self, draftName):
+        self.wait(By.CLASS_NAME, 'icon-edc-add')
+        self.click(By.CLASS_NAME, 'icon-edc-add')
+        self.wait(By.CLASS_NAME, 'headerExtra___3fjDw')
+        # eles = len(self.findElements(By.CSS_SELECTOR, '.headerExtra___3fjDw > li'))
+        # Create Manually
+        self.click(By.CSS_SELECTOR, '.list___3zSjf > li:nth-child(2)')
+        self.wait(By.ID, 'draftName')
+        self.findElement(By.ID, 'draftName').send_keys(draftName)
+        self.click(By.CSS_SELECTOR, '.edc-row-end > div > button')
+        time.sleep(0.5)
+
+    def assert_draft_name(self):
+        text = self.findElement(
+            By.CSS_SELECTOR,
+            '.wrap___2wsUC > .edc-form-horizontal > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(2)').text
+        print(text)
+        return text
+
+    def delete_draft(self):
+        # self.wait(
+        #     By.CSS_SELECTOR,
+        #     '.wrap___2wsUC > .edc-form-horizontal > div:nth-child(1)')
+        ele = self.findElement(By.CSS_SELECTOR, '.wrap___2wsUC > .edc-form-horizontal > div:nth-child(1) > div')
+
+        ele1 = self.findElement(By.CSS_SELECTOR,
+                                '.wrap___2wsUC > .edc-form-horizontal > div:nth-child(1) > div > div:nth-child(2)')
+        self.action_move(ele)
+        self.wait(By.CSS_SELECTOR,
+                  '.wrap___2wsUC > .edc-form-horizontal > div:nth-child(1) > div > div:nth-child(2)')
+        self.action_move(ele1)
+        print(self.findElements(By.CSS_SELECTOR,
+                                '.wrap___2wsUC > .edc-form-horizontal > div:nth-child(1) > div:nth-child(2)'))
+        return self.findElements(By.CSS_SELECTOR,
+                                 '.wrap___2wsUC > .edc-form-horizontal > div:nth-child(1) > div:nth-child(2)')
 
     def back(self):
         hover_ele = self.findElement(By.CLASS_NAME, 'back___Ras7T')
--- geektime_0-master/Itaimei_ui/tests/test_ins.py
+++ geektime_0-master/Itaimei_ui/tests/test_ins.py
@@ -4,6 +4,8 @@
 # @Email : 13952047994@163.com
 # @File : test_ins.py
 # @Project : geektime_0-master
+from logging import log, INFO
+import pytest
 from Itaimei_ui.page.sign import Sign
 
 
@@ -28,6 +30,15 @@ class TestIns(object):
         # self.sign.clearInput()
         ...
 
+    @pytest.mark.parametrize("draftName", [["draftName1"], ["draftName2"]])
+    def test_ins_add_draft(self, draftName):
+        self.toInfras.add_draft(draftName)
+        log(INFO, draftName[0])
+        assert draftName[0] in self.toInfras.assert_draft_name()
+
+    def test_delete_draft(self):
+        self.toInfras.delete_draft()
+
     def test_ins(self):
         self.toInfras.draftManagement()
         self.toInfras.toPlan()
@@ -42,3 +53,27 @@ class TestIns(object):
         self.toInfras.back()
         self.toInfras.toAnalytes()
         self.toInfras.back()
+        self.toInfras.toEcrfAccess()
+        self.toInfras.back()
+        self.toInfras.toGuidelines()
+        self.toInfras.back()
+        self.toInfras.toEditCheck()
+        self.toInfras.back()
+        self.toInfras.toDerivations()
+        self.toInfras.back()
+        self.toInfras.toAdvancedFunctions()
+        self.toInfras.back()
+        self.toInfras.toMappingConfig()
+        self.toInfras.back()
+
+    def test_ins_export(self):
+        ...
+
+    def test_ins_import(self):
+        ...
+
+    def test_ins_overwrite(self):
+        ...
+
+    def test_release(self):
+        ...
