cc9a47ba1dc99bfc93d653400ea1c0bdd064b932
zhoufan55 <1107707279@qq.com>
2023-01-11 18:29:21 +0800

    itamei1

--- geektime_0-master/itamei_api/mock/mitmlocal.py
+++ geektime_0-master/itamei_api/mock/mitmlocal.py
@@ -9,19 +9,18 @@ from mitmproxy import http
 
 
 def request(flow: http.HTTPFlow) -> None:
-    if 'id=2c9c812f82aec1300182b3c334f97fa7' in flow.request.pretty_url:
+    if 'id=8ac09f3684d096ba0184d0a9ee7f0461' in flow.request.pretty_url:
         # flow.response = http.Response.make(
         #     200,
         #     b'hello world',
         #     {"Content-Type": "application/json"}
         # )
-        # with open('hh.json') as f:
-        #     flow.response = http.Response.make(
-        #         200,
-        #         f.read(),
-        #         {"Content-Type": "application/json"}
-        #     )
-        flow.request.query['id'] = '8a8dbb5e857753ef0185948d8d54600e'
+        with open('hh.json') as f:
+            flow.response = http.Response.make(
+                200,
+                f.read(),
+                {"Content-Type": "application/json"}
+            )
 
 
 
